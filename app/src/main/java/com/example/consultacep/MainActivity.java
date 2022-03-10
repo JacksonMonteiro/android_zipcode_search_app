efffc7306eca4e0dcbff7694b1552757a682dfe0
jacksonmonteiro <infor.jackson324@gmail.com>
2022-03-10 16:23:05 -0300

    Splash screen

--- app/src/main/java/com/example/consultacep/MainActivity.java
+++ app/src/main/java/com/example/consultacep/MainActivity.java
@@ -2,13 +2,23 @@ package com.example.consultacep;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+
+import com.example.consultacep.activities.CepActivity;
 
 public class MainActivity extends AppCompatActivity {
+    private static final int TIMEOUT = 2000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        new Handler().postDelayed(() -> {
+            startActivity(new Intent(getApplicationContext(), CepActivity.class));
+            finish();
+        }, TIMEOUT);
     }
 }
---
+++ app/src/main/java/com/example/consultacep/activities/CepActivity.java
@@ -0,0 +1,16 @@
+package com.example.consultacep.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.consultacep.R;
+
+public class CepActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cep);
+    }
+}
